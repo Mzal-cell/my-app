d3118236837a9480d17afe1c2e2fe6a292ffd9d9
mzalendo <daimamzalendo5@gmail.com>
2026-03-06 11:35:01 +0300

    2 cmt

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Button from './components/Counter.jsx'
+import LikeButton from './components/Like.jsx'
+
+
+
+function App() {
+  return (
+    <>
+      <Button />
+    </>
+  )
+}
+
+export default App
+
